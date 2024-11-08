a09ea3a1901a211754ba7ab4a06f8cbecc057743
Anjali Narang <s1328134@monmouth.edu>
2024-11-07 19:22:39 -0500

    edit item view controller works

--- ToDo App/ViewControllers/EditItemViewController.swift
+++ ToDo App/ViewControllers/EditItemViewController.swift
@@ -12,6 +12,7 @@ class EditItemViewController: UIViewController {
     var showItemId: String?
     var selectedItem: ToDo?
     let toDoModel = ToDoModel.shared
+    let userModel = UserModel.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,10 +46,42 @@ class EditItemViewController: UIViewController {
     }
     
     @IBAction func switchValueChanged(_ sender: Any) {
+        
+        if let currentUser = userModel.currentUser {
+
+            if (completedSwitch.isOn) {
+                selectedItem?.completedBy = currentUser.username
+                selectedItem?.completedDateTime = getLongDateTime()
+            } else {
+                selectedItem?.completedBy = "N/A"
+                selectedItem?.completedDateTime = "N/A"
+            }
+            
+        }
+    
     }
     
     @IBAction func saveButtonPress(_ sender: Any) {
-        
+        if let item = selectedItem {
+            toDoModel.updateItem(item: item)
+        } else {
+            print("Cannot save")
+        }
+    }
+    
+    func timeInterval() -> String {
+        let tnow = Date()
+        var ts = String(tnow.timeIntervalSince1970)
+        ts = ts.replacingOccurrences(of: ".", with: "")
+        return ts
+    }
+    
+    func getLongDateTime() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .long
+        return dateFormatter.string(from: date)
     }
     
 }
